ea73b6b883d7e84e07e282fdfb39562a0467e38e
TFKusa <140806242+TFKusa@users.noreply.github.com>
2023-12-27 00:04:14 +0100

    Update kusa_Setup Trackspacer from selected to hovered track.lua

--- FX/kusa_Setup Trackspacer from selected to hovered track.lua
+++ FX/kusa_Setup Trackspacer from selected to hovered track.lua
@@ -1,7 +1,8 @@
 -- @description kusa_Setup Trackspacer from selected to hovered track
--- @version 1.0
+-- @version 1.01
 -- @author Kusa
--- @website https://thomashugofritz.wixsite.com/website
+-- @website PORTFOLIO : https://thomashugofritz.wixsite.com/website
+-- @website FORUM : https://forum.cockos.com/showthread.php?p=2745640#post2745640
 -- @donation https://paypal.me/tfkusa?country.x=FR&locale.x=fr_FR
 
 function main()
